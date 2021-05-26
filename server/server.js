bfa92ea0837be0887dfc777c5925a2e2bffcacb1
Naveen Saharan <74663372+nvnsaharan@users.noreply.github.com>
2021-05-26 10:00:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,64 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
 
-let data;
-
+let data = {};
+let data2 = {};
 CSVToJSON()
 	.fromFile('data.csv')
 	.then((users) => {
-		data = users;
+		for (let index = 0; index < users.length; index++) {
+			const element = users[index];
+			if (data2[element['Date;'].slice(0, 10)]) {
+				var date = element['Date;'].slice(0, 10);
+				data2[date].push({
+					Product: element['Product Name;'].slice(
+						0,
+						element['Product Name;'].length - 1
+					),
+					Bugs: Number(element['Number Of Bugs']),
+				});
+			} else {
+				var date = element['Date;'].slice(0, 10);
+				data2[date] = [
+					{
+						Product: element['Product Name;'].slice(
+							0,
+							element['Product Name;'].length - 1
+						),
+						Bugs: Number(element['Number Of Bugs']),
+					},
+				];
+			}
+			////////////////////////////
+			if (
+				data[
+					element['Product Name;'].slice(
+						0,
+						element['Product Name;'].length - 1
+					)
+				]
+			) {
+				var product = element['Product Name;'].slice(
+					0,
+					element['Product Name;'].length - 1
+				);
+				data[product].push({
+					date: element['Date;'].slice(0, 10),
+					Bugs: Number(element['Number Of Bugs']),
+				});
+			} else {
+				var product = element['Product Name;'].slice(
+					0,
+					element['Product Name;'].length - 1
+				);
+				data[product] = [
+					{
+						date: element['Date;'].slice(0, 10),
+						Bugs: Number(element['Number Of Bugs']),
+					},
+				];
+			}
+		}
 	})
 	.catch((err) => {
 		console.log(err);
@@ -16,5 +68,9 @@ CSVToJSON()
 
 app.get('/', (req, res) => {
 	res.json(data);
+});
+
+app.get('/date', (req, res) => {
+	res.json(data2);
 });
 app.listen(port);
